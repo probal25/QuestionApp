52bf3066e93072af7bb1c5952f3e7f29473b6b94
probal@divine-it.net <probal@divine-it.net>
2022-02-22 12:15:42 +0600

    Added Index Url mapping

---
+++ src/main/java/com/probal/examapp/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.probal.examapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String redirect() {
+        return "redirect:/question/all_questions";
+    }
+}
